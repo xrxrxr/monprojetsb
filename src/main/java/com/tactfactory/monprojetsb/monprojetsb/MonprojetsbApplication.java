7b43b9c9ec4fe8264ac414ce6df2a53553b95c41
xavier refour <xrthefrench@gmail.com>
2021-03-05 10:02:49 +0100

    first commit

---
+++ src/main/java/com/tactfactory/monprojetsb/monprojetsb/MonprojetsbApplication.java
@@ -0,0 +1,13 @@
+package com.tactfactory.monprojetsb.monprojetsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonprojetsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonprojetsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tactfactory/monprojetsb/monprojetsb/MonprojetsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.monprojetsb.monprojetsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonprojetsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
